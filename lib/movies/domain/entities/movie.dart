59df3810059254fff51dec440f5ac16be0c3e3cf
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-08-09 17:52:43 +0300

    apply equatable package on mocie class

---
+++ lib/movies/domain/entities/movie.dart
@@ -0,0 +1,22 @@
+import 'package:equatable/equatable.dart';
+
+class Movie extends Equatable {
+  final int id;
+  final String title;
+  final String overview;
+  final String posterPath;
+  final double voteAverage;
+  final List<int> genders;
+
+  const Movie({
+    required this.id,
+    required this.title,
+    required this.overview,
+    required this.posterPath,
+    required this.voteAverage,
+    required this.genders,
+  });
+  
+  @override
+  List<Object?> get props => [id, title, overview, posterPath, voteAverage, genders];
+}
