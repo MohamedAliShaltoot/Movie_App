21fe3fe6844a9fe36bb5b8223f4b2e654a9010af
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-08-16 00:26:50 +0300

    dadd movie model class

--- lib/movies/data/models/movie_model.dart
+++ lib/movies/data/models/movie_model.dart
@@ -1,23 +1,25 @@
+
+
 import 'package:movie_app/movies/domain/entities/movie.dart';
 
 class MovieModel extends Movie {
   const MovieModel({
     required super.id,
     required super.title,
+    required super.backdropPath,
+    required super.genreIds,
     required super.overview,
     required super.voteAverage,
-    required super.posterPath,
-    required super.genders,
     required super.releaseDate,
   });
 
   factory MovieModel.fromJson(Map<String, dynamic> json) => MovieModel(
-    id: json["id"],
-    title: json["title"],
-    posterPath: json["backdrop_path"],
-    genders: List<int>.from(json["genre_ids"].map((e) => e)),
-    overview: json["overview"],
-    voteAverage: json["vote_average"].toDouble(),
-    releaseDate: json["release_date"],
-  );
+        id: json["id"],
+        title: json["title"],
+        backdropPath: json["backdrop_path"],
+        genreIds: List<int>.from(json["genre_ids"].map((e) => e)),
+        overview: json["overview"],
+        voteAverage: json["vote_average"].toDouble(),
+        releaseDate: json["release_date"],
+      );
 }
